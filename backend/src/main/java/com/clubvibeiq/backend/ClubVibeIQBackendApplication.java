815d2cb97d565998787fd2d804b7f9fcbeb10812
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2025-04-23 13:56:58 +0530

    Initial commit

---
+++ backend/src/main/java/com/clubvibeiq/backend/ClubVibeIQBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clubvibeiq.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubVibeIQBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubVibeIQBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/clubvibeiq/backend/ClubVibeIQBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubvibeiq.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubVibeIQBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
